43bc31c03314df2eca578da24bdc04396113bf81
Emir Topaloglu <nonotelite@gmail.com>
2023-02-25 10:37:46 +0300

    Plinko Probability p1 version 2

--- Js Scripts/RandomBoxTest.js
+++ Js Scripts/RandomBoxTest.js
@@ -1,13 +1,48 @@
-const BOX_COUNT = 31;
+const BOX_COUNT = 5;
 const BALL_DROP = 99999999;
+
+const Factoriel = (n) => {
+  if (n === 0) {
+    return 1;
+  }
+
+  let sumFactoriel = 1;
+  for (let i = n; i > 0; i--) {
+    sumFactoriel *= i;
+  }
+
+  return sumFactoriel;
+};
+
+const Combination = (n, r) => {
+  return Factoriel(n) / (Factoriel(n - r) * Factoriel(r));
+};
+
+const idealResult = {};
+for (let i = 1; i < BOX_COUNT + 1; i++) {
+  idealResult[i] = Combination(BOX_COUNT - 1, i - 1);
+}
+
+let pascalTotal = 0;
+
+Object.values(idealResult).forEach((element) => {
+  pascalTotal += element;
+});
+const closestNumber = Math.floor(BALL_DROP / pascalTotal);
+
+Object.keys(idealResult).forEach((element) => {
+  idealResult[element] *= closestNumber;
+});
+
+console.log(idealResult);
 
 const LeftOrRight = (n, oddOrEven) => {
   let sumNumber = 0;
 
   if (oddOrEven) {
     for (let i = 0; i < n; i++) {
-      const randomNumber = Math.random() * 10 + 1;
-      if (randomNumber >= 6) {
+      const randomNumber = Math.random();
+      if (randomNumber >= 0.501) {
         sumNumber += 0.5;
       } else {
         sumNumber -= 0.5;
@@ -15,8 +50,8 @@ const LeftOrRight = (n, oddOrEven) => {
     }
   } else {
     for (let i = 0; i < n; i++) {
-      const randomNumber = Math.random() * 10 + 1;
-      if (randomNumber >= 6) {
+      const randomNumber = Math.random();
+      if (randomNumber >= 0.501) {
         sumNumber += 1;
       } else {
         sumNumber -= 1;
@@ -44,7 +79,17 @@ const RandomBoxExperiment = () => {
     }
   }
 
-  console.log(sumBoxes);
+  return sumBoxes;
 };
 
-RandomBoxExperiment();
+let sumBoxes = RandomBoxExperiment();
+
+console.log(sumBoxes);
+
+let sumDeflection = {};
+
+for (let i = 1; i < BOX_COUNT + 1; i++) {
+  sumDeflection[i] = idealResult[i] / sumBoxes[i] - 1;
+}
+
+console.log(sumDeflection);
--- Js Scripts/test.js
+++ Js Scripts/test.js
@@ -1,19 +1,14 @@
-let list1 = [1, 2, 3];
-let list2 = [4, 5, 6];
+let sumNumber1 = 0;
+let sumNumber2 = 0;
 
-let object1 = {
-  a: 1,
-  b: 2,
-  c: 3,
-  c: 4,
-};
+const repeatTime = 10000000;
 
-console.log(object1['a']);
+for (let i = 0; i < repeatTime; i++) {
+  sumNumber1 += Math.random() * 10;
+}
+for (let i = 0; i < repeatTime; i++) {
+  sumNumber2 += Math.floor(Math.random() * 10);
+}
 
-console.log('helo' + 1);
-
-const object2 = { 0.5: 1, 2: 4 };
-
-object2[2] += 1;
-
-console.log(object2);
+console.log(sumNumber1 / repeatTime + ' ' + sumNumber2 / repeatTime);
+console.log(6 > 6.1);
